89412da665fee84ff42e914b0d432fb45f692315
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-19 21:25:32 -0600

    add comment

--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -3,8 +3,17 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+# Function to set up the browser with the necessary configurations
 def setup_browser():
+    # Initialize the ChromeDriver service using WebDriverManager to handle driver binaries
     service = Service(ChromeDriverManager().install())
+    
+    # Create a new instance of the Chrome browser with the specified service
     driver = webdriver.Chrome(service=service)
+    
+    # Maximize the browser window for better visibility during tests
     driver.maximize_window()
+    
+    # Return the configured browser instance
     return driver
+
