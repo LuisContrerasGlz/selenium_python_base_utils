984d3c43cbef172ce09a3b072fd353d294c64dde
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-22 20:18:07 -0600

    progress

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from pages.sandbox_page import SandboxPage
 
 
+# Function to add custom command line options to pytest
 def pytest_addoption(parser):
     parser.addoption(
         "--browser",
@@ -18,10 +19,13 @@ def pytest_addoption(parser):
         help="Type of browser: chrome, firefox, edge, safari",
     )
 
-
+# Fixture to set up the browser instance based on the command line option
 @pytest.fixture(scope="session")
 def browser(request):
+    # Get the browser type from command line options
     browser_type = request.config.getoption("--browser").lower()
+    
+    # Set up the browser driver based on the browser type
     if browser_type == "chrome":
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service)
@@ -34,12 +38,14 @@ def browser(request):
     elif browser_type == "safari":
         driver = webdriver.Safari()
     else:
+        # Raise an error if the browser type is not supported
         raise ValueError(f"Browser {browser_type} not supported")
 
+    # Provide the WebDriver instance to the test and ensure cleanup
     yield driver
     driver.quit()
 
-
+# Fixture to provide an instance of the SandboxPage
 @pytest.fixture
 def sandbox_page(browser):
     return SandboxPage(browser)
